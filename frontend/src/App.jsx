aa86b0ed17b3492ee31878ae77322c0798040212
Colin Okafor <colinokafor116@gmail.com>
2025-12-02 08:51:11 -0500

    Fixed the footer import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
