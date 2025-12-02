58cd9371caff3a0be7bdcd07dfa333486813813f
Colin Okafor <colinokafor116@gmail.com>
2025-12-02 12:05:52 -0500

    new changes added to the config and package files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: "0.0.0.0",
-    port: 2005,  // fallback for local dev
-    allowedHosts: ["cafe-temp-frontend.onrender.com"]
-  }
+
 })
